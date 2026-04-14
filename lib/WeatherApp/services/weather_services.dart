cd2622b01424efbf9989af335366e8e702a2a800
Saurav Kumar <sauravkumar7678g@gmail.com>
2026-04-15 01:02:17 +0530

    Update API key to placeholder in weather_services.dart
    Replaced hardcoded API key with a placeholder.

--- lib/WeatherApp/services/weather_services.dart
+++ lib/WeatherApp/services/weather_services.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:my_basic_projects/WeatherApp/models/weather_model.dart';
 
 class WeatherServices{
-  final String apiKey= '9e921fff4cb7efb7a750b1e0d96dac86';
+  final String apiKey= 'API_KEY';
 
   Future<WeatherModel> featchWeather(String city) async{
     final url= Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey');
